095e8759d75eb84e0e4626a544496a412c914d3b
Ole <login15911591@gmail.com>
2020-01-21 11:43:25 +0200

    convert yearlyPaymentsPercentageRange ESCO

--- allbiz_service.py
+++ allbiz_service.py
@@ -168,10 +168,8 @@ def adapt_view_tender_data(value, field_name):
         value = int(value)
     elif 'agreementDuration' in field_name:
         value = "P{}".format(value.replace(u" роки, ", "Y").replace(u" місяця, ", "M").replace(u" днів", "D"))
-    # elif 'yearlyPaymentsPercentageRange' in field_name:
-    #     value = int(value)
-    # elif 'yearlyPaymentsPercentageRange'in field_name:
-    #     value = "{}"value.replace(u"від 0% до ", "")
+    elif 'yearlyPaymentsPercentageRange'in field_name:
+        value = float(value.split(" ")[-1][:-1]) / 100
     return convert_string_from_dict_allbiz(value)
 
 
